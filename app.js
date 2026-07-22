6f9c3ec067995747f212e96149a55b6d1834dfe1
ABID-107 <abidurrahman5000@gmail.com>
2026-07-22 15:08:01 +0600

    learning mongo db

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const userModel = require('./userModel');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// app.get('/create', async (req, res) => {
+//     let createdUser = await userModel.create({
+//         name: 'Abidji',
+//         username: 'abidji123',
+//         email: 'abidji@gmail.com'
+//     });
+//     res.json(createdUser);
+// });
+
+// app.get('/update', async (req, res) => {
+//     let updatedUser = await userModel.findOneAndUpdate(
+//         { username: 'abid123' },
+//         { name: 'Abid Khan' },
+//         { returnDocument: 'after' });
+//     res.json(updatedUser);
+// });
+
+// app.get('/read', async (req, res) => {
+//     let users = await userModel.find({username: 'abid123'});
+//     res.send(users);
+// });
+
+// app.get('/delete', async (req, res) => {
+//     let users = await userModel.findOneAndDelete({username: 'abid123'});
+//     res.send(users);
+// });
+
+app.listen(3000)
