63feddf284f1439539c6e132cae17ee98e291d04
ABID-107 <abidurrahman5000@gmail.com>
2026-07-22 17:51:44 +0600

    last update

---
+++ model/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/testApp1");
+
+const userSchema = mongoose.Schema({
+    image: String,
+    email: String,
+    name: String
+})
+
+module.exports = mongoose.model('user', userSchema);
